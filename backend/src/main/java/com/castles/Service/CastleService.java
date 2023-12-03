d7c7f17dad3e2295418a4321ffc91a04766ac99d
Jan Sterle <86469319+JanSterle1337@users.noreply.github.com>
2023-12-03 15:51:08 +0100

    Update CastleService.java

--- backend/src/main/java/com/castles/Service/CastleService.java
+++ backend/src/main/java/com/castles/Service/CastleService.java
@@ -34,7 +34,7 @@ public class CastleService {
     public List<Castle> scrapeCastles() {
         List<Castle> castles = new ArrayList<>();
 
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         System.setProperty("webdriver.http.factory", "jdk-http-client");
 
         WebDriver webDriver = new ChromeDriver(webDriverOptions.getChromeOptions());
